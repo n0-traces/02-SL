defe8c147382477ce1793b4284ece163a76363cd
xm <410516859@qq.com>
2025-09-16 15:00:33 +0800

    Update NFTDemo.sol

--- 02-workwork/homework02/NFTDemo.sol
+++ 02-workwork/homework02/NFTDemo.sol
@@ -1,1 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTDemo is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _tokenIdCounter;
+    
+    constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {}
+    
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+        _tokenIdCounter++;
+        uint256 tokenId = _tokenIdCounter;
+        
+        _safeMint(recipient, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        
+        return tokenId;
+    }
+    
+    function getCurrentTokenId() public view returns (uint256) {
+        return _tokenIdCounter;
+    }
+    
+    // 重写必要的函数以支持ERC721URIStorage
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+    
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
 
